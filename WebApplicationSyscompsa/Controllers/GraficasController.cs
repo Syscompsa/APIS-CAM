4050df5f215b57f96cdde47c6577ee5c7a42b537
syscompsa <Syscompsa.2020>
2020-07-23 17:58:31 -0500

    APIS - Gestion de siembras solo metodos [GET], obtencion de datos

---
+++ WebApplicationSyscompsa/Controllers/GraficasController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationSyscompsa.Models;
+
+namespace WebApplicationSyscompsa.Controllers
+{
+    [Route("api/AR_1-GraficasCamApp")]
+    [ApiController]
+    public class GraficasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public GraficasController(AppDbContext context)
+        {
+            this._context = context;
+            
+
+        }
+
+        [HttpGet]
+        [Route("sp_0314e/{siembra}")]
+        public ActionResult<DataTable> Getsp_0314e([FromRoute] string siembra)
+        {
+            string Sentencia = "exec sp_0314e @siembra, 1";
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(_context.Database.GetDbConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(Sentencia, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.SelectCommand.CommandType = CommandType.Text;
+                    //adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt == null)
+            {
+                return NotFound("");
+            }
+            return Ok(dt);
+        }
+
+        [HttpGet]
+        [Route("SP_GRAFICAWEB/{siembra}")]
+        public ActionResult<DataTable> GetSP_GRAFICAWEB([FromRoute] string siembra)
+        {
+            string Sentencia = "exec SP_GRAFICAWEB @siembra";
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(_context.Database.GetDbConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(Sentencia, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.SelectCommand.CommandType = CommandType.Text;
+                    //adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt == null)
+            {
+                return NotFound("");
+            }
+            return Ok(dt);
+        }
+
+        [HttpGet]
+        [Route("sp_datagraph/{siembra}")]
+        public ActionResult<DataTable> Sp_datagraph([FromRoute] string siembra)
+        {
+            string Sentencia = "exec sp_datagraph @siembra";
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(_context.Database.GetDbConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(Sentencia, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.SelectCommand.CommandType = CommandType.Text;
+                    //adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt == null)
+            {
+                return NotFound("");
+            }
+            return Ok(dt);
+            
+        }
+
+        [HttpGet]
+        [Route("sp_alimenproy/{siembra}")]
+        public ActionResult<DataTable> Sp_alimenproy([FromRoute] string siembra)
+        {
+            string Sentencia = "exec sp_alimenproy @siembra";
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(_context.Database.GetDbConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(Sentencia, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.SelectCommand.CommandType = CommandType.Text;
+                    //adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.SelectCommand.Parameters.Add(new SqlParameter("@siembra", siembra));
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt == null)
+            {
+                return NotFound("");
+            }
+            return Ok(dt);
+        }
+
+        [HttpGet]
+        [Route("GetGenView")]
+        public ActionResult<DataTable> GetGenView()
+        {
+
+            string Sentencia = "select A.codigo, A.fecha, A.fechaven, A.camaron, C.nombre as nomcamaronera,A.piscina, B.nombre as nompiscina" +
+                                " from dp03a188 A left join dp13a110 C ON A.camaron = C.cod_cam left join dp03a130 B ON b.codigo = A.piscina" +
+                                " where fechaven > GETDATE()";
+
+            DataTable dt = new DataTable();
+            using (SqlConnection connection = new SqlConnection(_context.Database.GetDbConnection().ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(Sentencia, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    adapter.Fill(dt);
+                }
+            }
+
+            if (dt == null)
+            { return NotFound(""); }
+
+            return Ok(dt);
+        }
+
+    }
+}
---
+++ WebApplicationSyscompsa/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApplicationSyscompsa.Models;
+
+namespace WebApplicationSyscompsa
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers();
+
+            var connection = @"Data Source = 181.196.189.58,59925\\SQLEXPRESS; Initial Catalog = SQLOSTRATEK; Persist Security Info = True; User ID = sa; Password = Rootpass1";
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:58726", "http://www.syscompsa.somee.com/")
+            .AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
